8e7c5c8b503764ce2a02d9589ffac189b8fb45cc
Tinh <nguyentinh14032001@gmail.com>
2022-05-17 15:50:11 +0700

    first commit

---
+++ app/src/main/java/com/diendanbinhdinh/projectandroidcuoiki/activities/DangKyActivity.java
@@ -0,0 +1,16 @@
+package com.diendanbinhdinh.projectandroidcuoiki.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.diendanbinhdinh.projectandroidcuoiki.R;
+
+public class DangKyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dang_ky);
+    }
+}
---
+++ app/src/main/java/com/diendanbinhdinh/projectandroidcuoiki/activities/DangNhapActivity.java
@@ -0,0 +1,17 @@
+package com.diendanbinhdinh.projectandroidcuoiki.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.diendanbinhdinh.projectandroidcuoiki.R;
+
+public class DangNhapActivity extends AppCompatActivity {
+private com.diendanbinhdinh.projectandroidcuoiki.databinding.ActivityDangNhapBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dang_nhap);
+    }
+}
